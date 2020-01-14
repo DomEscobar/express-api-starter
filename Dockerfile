cbbfc73cee7cc7f3e072f9681b5fcef2dc42335c
domEscobar <admin@changeisamazing.com>
2020-01-14 20:41:55 +0100

    change

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM node:alpine
 
+RUN apk add --nocache udev ttf-freefont chromium git
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+ENV CHROMIUM_PATH /usr/bin/chromium-browser
+
 # Create app directory
 WORKDIR /usr/src/app
 
--- app/api/youtube.js
+++ app/api/youtube.js
@@ -14,7 +14,7 @@ router.get('/click', async (req, res) => {
 
         for (let proxy of global.proxyList) {
 
-            const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, acceptInsecureCerts: true, args: ['--proxy-server=' + proxy.ip + ':' + proxy.port, '--proxy-bypass-list=*', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-first-run', '--no-sandbox', '--no-zygote', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', '--enable-features=NetworkService'] })
+            const browser = await puppeteer.launch({   executablePath: process.env.CHROMIUM_PATH, ignoreHTTPSErrors: true, acceptInsecureCerts: true, args: ['--proxy-server=' + proxy.ip + ':' + proxy.port, '--proxy-bypass-list=*', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-first-run', '--no-sandbox', '--no-zygote', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', '--enable-features=NetworkService'] })
             const page = await browser.newPage()
             await page.goto(link, { waitUntil: 'load', timeout: 0 })
             const selector = '.ytp-time-current';
