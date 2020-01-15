4fa0f7a7ad3899cec01321de283e230864eba183
domEscobar <admin@changeisamazing.com>
2020-01-15 22:01:34 +0100

    update youtube

--- app/api/youtube.js
+++ app/api/youtube.js
@@ -13,17 +13,13 @@ router.get('/click', async (req, res) => {
         }
 
         for (let proxy of global.proxyList) {
-
+            
             const browser = await puppeteer.launch({ headless:true, ignoreHTTPSErrors: true, acceptInsecureCerts: true, args: ['--proxy-server=' + proxy.ip + ':' + proxy.port, '--proxy-bypass-list=*', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--no-first-run', '--no-sandbox', '--no-zygote', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list', '--enable-features=NetworkService'] })
             const page = await browser.newPage()
             await page.goto(link, { waitUntil: 'load', timeout: 0 })
             const selector = '.ytp-time-current';
-            await page.waitForFunction(
-                selector => document.querySelector(selector).innerText ==  '0:31',
-                {timeout : 0},
-                selector
-            );
-            console.log("finwatching");
+            console.log("watch" + proxy.ip);
+            await page.waitFor(40000);
             await browser.close()
         }
         res.json('fin');
